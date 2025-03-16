be803dd9b109d6ba036abfda0bf80c13a8f70b17
Blortski <tabin68@gmail.com>
2025-03-15 20:19:24 -0600

    Fix GitHub token initialization for in-progress surveys (v1.2.8)

--- js/lib/config.js
+++ js/lib/config.js
@@ -18,7 +18,7 @@ const config = {
     },
     
     // Application version
-    version: 'v1.2.7'
+    version: 'v1.2.8'
 };
 
 // Export the configuration
--- js/survey.js
+++ js/survey.js
@@ -5,7 +5,7 @@
  * including loading questions, collecting responses, calculating scores,
  * and displaying results.
  * 
- * Version: v1.2.7
+ * Version: v1.2.8
  */
 
 // Global variables
